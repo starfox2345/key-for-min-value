63dc62b9a731b2f5be79a0cbd31f6651b063da5c
yatsun <yatsunlam25@gmail.com>
2021-03-05 22:44:44 -0500

    commit tree b65b966295aa8cc45db19e77739a57fff01aa946

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+def key_for_min_value(name_hash)   
+    low_key = nil
+    low_value = 0
 
-def key_for_min_value(name_hash)
-
+    name_hash.each do |key, value|
+        if low_value == 0 || low_value > value
+            low_value = value
+            low_key = key
+        end
+    end
+    low_key
 end
